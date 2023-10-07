e62e9d413618527a5f55480e4af281f531749e9b
nikhilchopra08 <129540378+nikhilchopra08@users.noreply.github.com>
2023-10-07 19:05:05 +0530

    2

--- src/Componets/Header.jsx
+++ src/Componets/Header.jsx
@@ -1,27 +1,3 @@
-// import React from "react";
-// import { Link } from "react-router-dom";
-// import { FiShoppingBag } from "react-icons/fi";
-// import { useSelector } from "react-redux";
-
-// const Header = () => {
-//   const { cartItems } = useSelector((state) => state.cart);
-//   return (
-//     <nav>
-//       <h2>Logo Here.</h2>
-
-//       <div>
-//         <Link to={"/"}>Home</Link>
-//         <Link to={"/cart"}>
-//           <FiShoppingBag />
-//           <p>{cartItems.length}</p>
-//         </Link>
-//       </div>
-//     </nav>
-//   );
-// };
-
-// export default Header;
-
 import React from 'react'
 import { Link } from 'react-router-dom'
 import {FiShoppingBag} from 'react-icons/fi'
@@ -37,23 +13,14 @@ const Header = () => {
     <nav>
       <div className='content'>
         <h2>logo here..</h2>
-        <div className="hidden-content">
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro est voluptas optio aut possimus incidunt cum pariatur enim, dolorem ducimus minima? Impedit saepe magni consectetur nemo dolores eos quas quaerat.</p>
-                  <Home/>
-                        </div>
-                        </div>
+          </div>
 
         <div>
+        <input type="text" placeholder="Search.."/>
             <Link to={"/"}>Shop All</Link>
             <Link to={"/"}>Home</Link>
             <Link to={"/"}>Handmade Crafts</Link>
-            <div className="login">
-            <button>Login</button>
-            <div className="hidden-content">
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro est voluptas optio aut possimus incidunt cum pariatur enim, dolorem ducimus minima? Impedit saepe magni consectetur nemo dolores eos quas quaerat.</p>
-              {/* <BasicExample/> */}
-                        </div>
-                        </div>
+            <Link to={"/"}>Login</Link>
             <Link to={"/cart"}>
                 <FiShoppingBag/>
                 <p>{cartItems.length}</p>
