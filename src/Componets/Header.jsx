fed3becdc419877e3f29890e5fb69dceb526d46d
nikhilchopra08 <129540378+nikhilchopra08@users.noreply.github.com>
2023-10-07 20:36:20 +0530

    finalCommit

--- src/Componets/Header.jsx
+++ src/Componets/Header.jsx
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom'
 import {FiShoppingBag} from 'react-icons/fi'
 import { useSelector } from 'react-redux'
 import Home from './Home'
-import BasicExample from './BasicExample'
 
 const Header = () => {
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -8,14 +8,3 @@ const store = configureStore({
 });
 
 export default store;
-
-// import {configureStore} from "@reduxjs/toolkit";
-// import { cartReducer } from "./reducer";
-
-// const store = configureStore({
-//    reducer: {
-//     cart: cartReducer
-//    },
-// });
-
-// export default store;
