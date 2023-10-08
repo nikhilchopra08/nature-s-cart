e55da3acc3a94882763af320311c55ff3098cc23
nikhilchopra08 <129540378+nikhilchopra08@users.noreply.github.com>
2023-10-08 16:32:21 +0530

    responsive

--- src/Componets/Header.jsx
+++ src/Componets/Header.jsx
@@ -5,12 +5,40 @@ import { useSelector } from 'react-redux'
 import Home from './Home'
 import logo from "./logo.png"
 
+
 const Header = () => {
 
   const {cartItems} = useSelector(state=>state.cart)
   return (
     <>
     <nav>
+      <div className="left">
+      <img src={logo}/>
+      </div>
+      <div className="other">
+
+        
+   <ul class="nav">
+  <li class="nav-item">
+  <Link to={"/"}>Home</Link>
+  </li>
+  <li class="nav-item">
+  <Link to={"/shop"}>Shop</Link>
+  </li>
+  <li class="nav-item">
+  <Link to={"/"}>Login</Link>
+  </li>
+  <li class="nav-item">
+  <Link to={"/cart"}>
+                <FiShoppingBag/>
+                <p>{cartItems.length}</p>
+            </Link>
+  </li>
+</ul>
+</div>
+</nav>
+   
+    {/* <nav>
       <div className='content'>
        <img src={logo}/>
 </div>
@@ -25,7 +53,7 @@ const Header = () => {
                 <p>{cartItems.length}</p>
             </Link>
         </div>
-    </nav>
+    </nav> */}
     </>
   )
 }
